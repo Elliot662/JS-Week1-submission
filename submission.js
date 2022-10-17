e7f6e89f1f6ab1e3ec1d68fe6bc730959fc3e388
Elliot662 <elliotmeltzer@hotmail.co.uk>
2022-10-17 09:53:17 +0100

    first commit

---
+++ submission.js
@@ -0,0 +1,44 @@
+// // 1.
+// let name1 = "Chris";
+// let age = 27;
+// let favColour = "blue"
+// console.log(`Hi, my name is ${name1}. I am ${age} and my favourite colour is ${favColour}.`)
+// age = 28;
+// favColour = "red"
+// console.log(`Hi, my name is ${name1}. I am ${age} and my favourite colour is ${favColour}.`);
+
+
+// // 2.
+// const films = ["The Dark Knight", "Puss in Boots", "Hotel Transylvania", "Host"]
+// console.log(films)
+// films.push("Chernobyl")
+// films.push("Inside Out")
+// console.log(films)
+// for (let i = 0; i < films.length; i++){
+//         console.log(films[i])}   
+
+
+// // 3.
+// let num = 22
+// if(num % 5 == 0 && num % 3 == 0){
+//     console.log("Fizz BUZZ")
+// }else if(num % 5 == 0){
+//     console.log("Buzz")
+// }else if(num % 3 == 0){
+//     console.log("Fizz")
+// }else{
+//     console.log(num + " booo")
+// }
+
+
+// // 4.
+// for(let numbers = 9; numbers > -1; numbers--)
+// console.log(numbers)
+
+
+// // 5.
+// const d = new Date();
+// let b = new Date(2022, 10, 24);
+// b -=d
+// b /=86400000
+// console.log(Math.ceil(b) + " days untill my birthday!")
